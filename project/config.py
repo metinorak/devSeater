08975a8d39ccd7a1b16d3d7363afd2242650d524
Metin Orak <orakmetin@gmail.com>
2020-01-30 03:22:31 +0300

    upload folder edited

--- project/config.py
+++ project/config.py
@@ -14,7 +14,7 @@ MAIL_PASS = ""
 SITE_ADDR = "http://localhost:5000"
 
 #FILE UPLOAD PATH
-UPLOAD_FOLDER = "static/uploads"
+UPLOAD_FOLDER = "project/static/uploads"
 
 #ALLOWED EXTENSIONS
 ALLOWED_PHOTO_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
